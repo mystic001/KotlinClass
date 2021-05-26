9bff635a3882c56c0fb2c6b88f92a5a7b2de18eb
mystic <bamideleomonayin@gmail.com>
2021-05-26 15:56:57 +0000

    experiment

---
+++ src/main/kotlin/Cat.kt
@@ -0,0 +1,21 @@
+class Cat(var name : String? = "") {
+
+    fun Meow(){
+        println("Meow")
+    }
+}
+
+//This is for an activity in cahpter 8
+fun main(){
+    val container = arrayOf(Cat("Misty"),null,Cat("Socks"))
+
+    for(x in container){
+
+        if(x != null ){
+            print("${x.name} ")
+            x.Meow()
+        }
+
+
+      }
+}
--- src/main/kotlin/chapter8.kt
+++ src/main/kotlin/chapter8.kt
@@ -13,23 +13,38 @@ fun main(){
     val s = d?.x ?: "boys"
     val str : String? = "Pizza";
 
-    //defining a parameter
+    println(printingBlue(4))
+    println(printlnBlack("yellow"))
+    println(res(4.0,6.0))
 
-    fun printingBlue( x :Int?) : String = "blue"
+}
 
-    fun printlnBlack(color: String?): String? {
 
-        val col = color
+//defining a parameter
 
-        return col
+fun printingBlue( x :Int?) : String = "blue"
+
+fun printlnBlack(color: String?): String? {
+
+    val col = color
+
+    return col
+}
+
+
+//defining functions to be nullable types
+
+fun res(x: Double, y: Double) : Double? = (x+y)
+
+
+class MyWolf(){
+    private var w : Wolf? = Wolf()
+
+
+    fun myFunction(){
+        w?.eat()
     }
 
 
-    //defining functions to be nullable types
-
-    fun res() : Int?{
-
-        return 56
-    }
 
 }
